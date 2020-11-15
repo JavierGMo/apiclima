b88ce6e8c9755192fa9e9b40282514d046a61d24
JavierGMo <jinjuriki62@gmail.com>
2020-11-15 17:54:46 -0600

    Weather first

---
+++ README.md
@@ -0,0 +1,6 @@
+# Consumo de API con Node.js
+
+Mediante Node.js se consume la API de [metaweather](https://www.metaweather.com), se procesa la data para ser consumida de nuevo por alguien mas.
+
+Con nodemon en el localhost
+`npm run mon`
---
+++ server/server.js
@@ -0,0 +1,69 @@
+// const { default: Axios } = require('axios');
+const express = require('express');
+const app = express();
+
+const axios = require('axios');
+
+const PORT = process.env.PORT || '9000';
+
+
+app.get('/weatherplace', async function(req, res){
+    
+    const location = req.query.location;
+    try {
+        const nextDays = [];
+        const weatherWoeid = await axios.get(`https://www.metaweather.com/api/location/search/?query=${location}`);
+        const data = weatherWoeid.data;
+        
+        // if(data === undefined || data === null) res.status(500).json({ok : false, data : 'no-data'});
+        const woeid = data[0]['woeid'];
+        
+        const weatherData = await axios.get(`https://www.metaweather.com/api/location/${woeid}`);
+        
+        //DayLetter, DayNumber MonthLetter
+        const today = {
+            'img_abbr' : weatherData.data['consolidated_weather'][0]['weather_state_abbr'],
+            'the_temp' : Math.round(weatherData.data['consolidated_weather'][0]['the_temp']),
+            'min_temp' : Math.round(weatherData.data['consolidated_weather'][0]['min_temp']),
+            'max_temp' : Math.round(weatherData.data['consolidated_weather'][0]['max_temp']),
+            'place' : weatherData.data['title'],
+            'weather_state_name' : weatherData.data['consolidated_weather'][0]['weather_state_name'],
+            'wind_speed' : Math.round(weatherData.data['consolidated_weather'][0]['wind_speed']),
+            'wind_direction_compass' : weatherData.data['consolidated_weather'][0]['wind_direction_compass'],
+            'humidity' : Math.round(weatherData.data['consolidated_weather'][0]['humidity']),
+            'visibility' : Math.round(weatherData.data['consolidated_weather'][0]['visibility']),
+            'air_pressure' : Math.round(weatherData.data['consolidated_weather'][0]['air_pressure'])
+        };
+
+        
+
+        for (let index = 0; index < 5; index++) {
+            
+            nextDays.push({
+                'img_abbr' : weatherData.data['consolidated_weather'][index]['weather_state_abbr'],
+                'the_temp' : Math.round(weatherData.data['consolidated_weather'][index]['the_temp']),
+                'min_temp' : Math.round(weatherData.data['consolidated_weather'][index]['min_temp']),
+                'max_temp' : Math.round(weatherData.data['consolidated_weather'][index]['max_temp'])
+            });
+            
+        }
+    
+        res.status(200).json({
+            ok : true,
+            data : {dataToday : today, dataNextDays : nextDays}
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            ok : false,
+            data : 'no-res'
+        });
+    }
+    
+});
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Running...`);
+});
